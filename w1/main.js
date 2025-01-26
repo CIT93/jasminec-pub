8a816d0beb3b60c8c4ba28ed4d25626a5c0d67f7
Jasmine Chang <jailiahc13@gmail.com>
2025-01-26 12:25:10 -0800

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,22 @@
-console.log("Week 1 - Hello World");
+console.log("hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "string";
+const myVarType = typeof(myVar);
+
+console.log("myVarType" + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log(`will this one run? 24`)
+    } else {
+        console.log(`will this one run?251`)
+    }
+}
+
+runNow();
+runNow();
