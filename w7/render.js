052c1ae54792c0d7cd41dc8ae8e24c3266ad0549
Jasmine Chang <jailiahc13@gmail.com>
2025-02-27 13:40:43 -0800

    my attempt at the code, before I review other students code

--- w7/render.js
+++ w7/render.js
@@ -4,7 +4,7 @@ function renderTblHeading () {
     const table = document.createElement("table");
     const thead = document.createElement("thead");
     const tr = document.createElement("tr");
-    const headingTextArr = ["Name", "HouseHold", "HouseSize", "Footprint", "Actions"];
+    const headingTextArr = ["Name", "Cfp Total"];
     headingTextArr.forEach(function(text){
         const th = document.createElement("th");
         th.textContent = text;
@@ -20,22 +20,25 @@ function renderTb1(data) {
     const table = renderTblHeading();
     const tbody = document.createElement("tbody");
     const tr = document.createElement("tr");
-    const trTextArr = ["Jasmine", 3, "Apt", 12];
-    trTextArr.forEach(function(text){
-      const td = document.createElement("td");
-      td.textContent = text;
-      tr.appendChild(td);
+    data.forEach(function(text){
+        const tr = document.createElement("tr");
+        const tdName = document.createElement("td");
+        tdName.textContent = text.name;
+        const tdTotal = document.createElement("td");
+        tdTotal.textContent = text.total;
+        tbody.appendChild(tr);
+        tr.appendChild(tdName);
+        tr.appendChild(tdTotal);
     });
     const td = document.createElement("td");
-    const btnEdit = document.createElement("button");
-    const btnDel = document.createElement("button");
-    btnEdit.textContent = "Edit";
-    btnDel.textContent = "Del";
+    // const btnEdit = document.createElement("button");
+    // const btnDel = document.createElement("button");
+    // btnEdit.textContent = "Edit";
+    // btnDel.textContent = "Del";
 
-    td.appendChild(btnEdit);
-    td.appendChild(btnDel);
+    // td.appendChild(btnEdit);
+    // td.appendChild(btnDel);
     tr.appendChild(td);
-    tbody.appendChild(tr);
     table.appendChild(tbody);
     console.log(table);
 
