3cf1f012e205b0d2de4f0b678823d56ae5d60d61
Jasmine Chang <jailiahc13@gmail.com>
2025-04-02 14:55:39 -0700

    week 12 solution

--- w12/main.js
+++ w12/main.js
@@ -1,6 +1,30 @@
 const FORM = document.getElementById("form");
 const output = document.getElementById("output");
 
+// Rios code
+// function updateDOM(message, el) {
+//     const newEl = document.createElement(el)
+//     newEl.textContent = message
+//     output.appendChild(newEl)
+// }
+
+// const startWorkout = (exerciseType, numOfReps, exerciseTime, callback) => {
+//     updateDOM(`Start ${exerciseType} <> Goal reps is ${numOfReps} <> Complete in ${exerciseTime} min!`, "p") // sync
+//     setTimeout(()=>{
+//         callback(`Stop ${exerciseType}`, "h1") // async
+//     }, exerciseTime * 1000)
+// }
+
+// FORM.addEventListener('submit', e => {
+//     e.preventDefault();
+//     const exerciseType = e.target.exerciseType.value
+//     const numOfReps = parseFloat(e.target.numOfReps.value)
+//     const exerciseTime = parseFloat(e.target.exerciseTime.value)
+//     startWorkout(exerciseType, numOfReps, exerciseTime, updateDOM)
+//     FORM.reset()
+// })
+
+// MY code
 function fitSubmission() {
   const heading = document.createElement("h2");
   heading.textContent = "Submit to start!";
@@ -26,4 +50,7 @@ function fitSubmission() {
 }
 
 fitSubmission();
+
+
+
 
