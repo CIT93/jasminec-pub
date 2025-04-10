57271ca6533e2c2958a0c7145189a7b4cc772fa2
Jasmine Chang <jailiahc13@gmail.com>
2025-04-09 17:07:42 -0700

    w13 code challenge

---
+++ w13/main.js
@@ -0,0 +1,77 @@
+const FORM = document.getElementById("form");
+const output = document.getElementById("output");
+
+// Rios code
+// function updateDOM(message, el) {
+//     const newEl = document.createElement(el)
+//     newEl.textContent = message
+//     output.appendChild(newEl)
+// }
+
+// const startWorkout = (exerciseType, numOfReps, exerciseTime, callback) => {
+//     updateDOM(`Start ${exerciseType} <> Goal reps is ${numOfReps} <> Complete in ${exerciseTime} min!`, "p") // sync
+//     setTimeout(()=>{
+//         callback(`Stop ${exerciseType}`, "h1") // async
+//     }, exerciseTime * 1000)
+// }
+
+// FORM.addEventListener('submit', e => {
+//     e.preventDefault();
+//     const exerciseType = e.target.exerciseType.value
+//     const numOfReps = parseFloat(e.target.numOfReps.value)
+//     const exerciseTime = parseFloat(e.target.exerciseTime.value)
+//     startWorkout(exerciseType, numOfReps, exerciseTime, updateDOM)
+//     FORM.reset()
+// })
+
+// MY code
+function fitSubmission(exerciseType, exerciseTime, numOfReps) {
+  const heading = document.createElement("h2");
+  heading.textContent = "Submit to start!";
+  output.appendChild(heading);
+
+  return new Promise((resolve, reject) => {
+    if (!exerciseType || !exerciseTime || !numOfReps) {
+      reject("Error: No exercise, time, or reps added.");
+    } else {
+      setTimeout(() => {
+        const message = document.createElement("p");
+        message.textContent = `Stop ${exerciseType}.`;
+        output.appendChild(message);
+        FORM.reset();
+        resolve();
+      }, 2000);
+    }
+  });
+}
+
+function onError(errorCode) {
+  const errorMsg = document.createElement("p");
+  errorMsg.textContent = `${errorCode}`;
+  output.appendChild(errorMsg);
+  FORM.reset();
+}
+
+FORM.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const exerciseType = FORM.exerciseType.value;
+  const numOfReps = FORM.numOfReps.value;
+  const exerciseTime = FORM.exerciseTime.value;
+
+  const message = document.createElement("p");
+  message.textContent = `Start ${exerciseType} for ${exerciseTime} minute(s) < > Goal reps is ${numOfReps}.`;
+  output.appendChild(message);
+
+  fitSubmission(exerciseType, exerciseTime, numOfReps)
+    .catch(onError);
+});
+
+
+
+
+
+
+
+
+
